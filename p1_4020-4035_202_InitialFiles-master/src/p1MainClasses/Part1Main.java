0904f82080f1e1e783608bb968861f20b068adbe
JVentosa <jose.ventosa1@upr.edu>
2021-03-13 17:51:42 -0400

    Part 1 Main, Input Files Scanners done. All Imports added. FinalSet left

--- p1_4020-4035_202_InitialFiles-master/src/p1MainClasses/Part1Main.java
+++ p1_4020-4035_202_InitialFiles-master/src/p1MainClasses/Part1Main.java
@@ -1,5 +1,51 @@
 package p1MainClasses;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import dataGenerator.DataReader;
+import setIntersectionFinders.AbstractIntersectionFinder;
+import setIntersectionFinders.IntersectionFinder_P1P2;
+import setIntersectionFinders.IntersectionFinder_P2;
+import setIntersectionFinders.IntersectionFinder_P3;
 public class Part1Main {
+	public static void main(String[] args) throws FileNotFoundException, CloneNotSupportedException {
 
+		int param, company, crime;
+
+		String parentDirectory = "inputFiles";
+		Scanner parameters = new Scanner(new File(parentDirectory, "parameters.txt"));
+		crime = parameters.nextInt();
+		company = parameters.nextInt();
+
+		parameters.close();
+
+		Integer[][][] dr = new Integer[company][crime][5000];
+
+		for (int i = 0; i < company; i++) {
+			for (int j = 0; i < crime; j++) {
+				Scanner file = new Scanner(new File("inputFiles/F_" + i + "_" + j + ".txt"));
+				int l = 0;
+				while (file.hasNext()) {
+					String next = file.nextLine();
+					int nextin = Integer.parseInt(next);
+					dr[i][j][l] = nextin;
+					l++;
+				}
+			}
+		}
+		
+//		AbstractIntersectionFinder<E> p1 = new IntersectionFinder_P1P2<E>("p1");
+//		AbstractIntersectionFinder<E> p2 = new IntersectionFinder_P1P2<E>("p2");
+//		AbstractIntersectionFinder<E> p3 = new IntersectionFinder_P2<E>();
+//		AbstractIntersectionFinder<E> p4 = new IntersectionFinder_P3<E>();
+		
+		if (args.length > 1) {
+			System.out.println("Unexpected number of parameters. Must be <= 1.");
+			return;
+		}
+		if (args.length == 1)
+			param = Integer.parseInt(args[0]);
+
+	}
 }
