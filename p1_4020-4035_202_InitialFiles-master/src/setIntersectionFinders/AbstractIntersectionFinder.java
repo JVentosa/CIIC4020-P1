a61c137e9f82b1cb3ebe4f6010a463af6fe8cbbd
JVentosa <jose.ventosa1@upr.edu>
2021-03-14 00:50:59 -0400

    Part 1 Main Done, P1P2 Done.

--- p1_4020-4035_202_InitialFiles-master/src/setIntersectionFinders/AbstractIntersectionFinder.java
+++ p1_4020-4035_202_InitialFiles-master/src/setIntersectionFinders/AbstractIntersectionFinder.java
@@ -1,15 +1,48 @@
 package setIntersectionFinders;
 
+import java.util.ArrayList;
 import interfaces.IntersectionFinder;
+import interfaces.MySet;
 
-public abstract class AbstractIntersectionFinder<E> 
-implements IntersectionFinder<E> {
-	private String name;   // to identify the strategy
+public abstract class AbstractIntersectionFinder<E> implements IntersectionFinder<E> {
+	private String name;
+
 	public AbstractIntersectionFinder(String name) {
-		this.name = name; 
+		this.name = name;
 	}
 
-	public String getName() { 
-		return name; 
+	public AbstractIntersectionFinder() {
+		this.name = name;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public MySet<E> intersectSets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MySet<E> intersectSets(MySet<E>[] t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MySet<E>[] createSets(Integer[][][] rawData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Integer> intersectMySets(ArrayList<Integer>[] mySet) {
+		return null;
+		// TODO Auto-generated method stub
+
+	}
+
+	public MySet<E>[] createSets2(Integer[][][] myData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
